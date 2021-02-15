aadeebbe3f35b298bf0dc06396228240e31800ea
Fernando Maia <fermaiasoares@aol.com>
2021-02-15 00:44:56 -0300

    Configuraçoes de Sign Code e Code Push

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import codePush, { CodePushOptions } from 'react-native-code-push';
 
 // import { Container } from './styles';
+
+const codePushOptions: CodePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  installMode: codePush.InstallMode.ON_NEXT_RESUME
+}
 
 const App: React.FC = () => {
   return <View />;
 }
 
-export default App;
+export default codePush()(App);
